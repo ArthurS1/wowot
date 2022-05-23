e4c4bdb725e7c5b7d32083c81f3c4317b2b0f1cf
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-23 21:12:29 +0200

    Add basic bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("yoyele")
+import { Client, Intents, TextChannel} from 'discord.js';
+import { token } from '../config.json';
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+  sendHello()
+});
+
+client.login(token)
+.then(token => {
+  console.log(`token in use is ${token}`);
+});
+
+function sendHello() {
+  const channel = client.channels.cache.get('798678617840287755');
+
+  if (channel?.isText) {
+    (channel as TextChannel).send("Bonjour, je suis wowot :wave:")
+      .then(_ => console.log('message sent'))
+      .catch(err => console.log(`message failed with error: ${err}`));
+  }
+}
+
