ace59d731071567a4e0011885124214adbe443ab
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-23 21:12:29 +0200

    Add basic bot

--- build/index.js
+++ build/index.js
@@ -1,1 +1,9 @@
-console.log("yoyele");
+"use strict";
+exports.__esModule = true;
+var discord_js_1 = require("discord.js");
+var config_json_1 = require("../config.json");
+var client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
+client.once('ready', function () {
+    console.log('Ready!');
+});
+client.login(config_json_1.token);
