8c4747e84d0731bf203b2c416a02f7cd087208ad
Arthur Soulié <arthur.soulie@tuta.io>
2022-05-28 21:16:08 +0200

    Add node-cron usage, cli & refactor
    The project now tries to be a bit more OO to
    please Morian Doy. A cli to quickly switch between
    debug and production was introduced.

---
+++ src/conf.ts
@@ -0,0 +1,8 @@
+interface Conf {
+  cron: string;
+  token: string;
+  debug_channel: string;
+  production_channel: string;
+}
+
+export default Conf;
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,14 @@
-import { Client, Intents, TextChannel, Message} from 'discord.js';
-import { token, channels } from '../config.json';
-import { LocalTime } from '@js-joda/core';
+import { Core, ServerState} from './core';
+import * as config from '../config.json';
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-client.once('ready', () => {
-	console.log('Working : ' + LocalTime.now());
-  sendTo(channels['debug'], 'Wowo les amis :wave:')
-    .then(response => {
-      console.log(response);
-	    console.log('Ending : ' + LocalTime.now());
-      process.exit()
-    })
-    .catch(err => {
-      console.log(err)
-	    console.log('Ending with error : ' + LocalTime.now());
-      process.exit(1)
-    });
-});
-
-client.login(token)
-.then(token => {
-  console.log(`token in use is ${token}`);
-});
-
-async function sendTo(channelId: string, msg: string): Promise<Message> {
-  const channel = client.channels.cache.get(channelId);
-
-  debugger
-  if (channel?.isText) {
-    return (channel as TextChannel).send(msg)
-  }
-  return Promise.reject("not a text channel")
+switch (process.argv[2]) {
+case '--prod':
+    new Core(config, ServerState.Production);
+    break;
+case '--help':
+    Core.showUsage();
+    break;
+default:
+    new Core(config, ServerState.Debug);
+    break;
 }
-
